b1856e4e896500bd1f146b4cac4edac5d65421bf
tinoh9 <bachhuynh959@gmail.com>
2023-02-18 16:03:43 +0700

    updated test

---
+++ problem-1/target/debug/deps/problem_1-b0a11966b1dfda56.d
@@ -0,0 +1,5 @@
+C:\Users\tino\Documents\GitHub\crack-code-rs\problem-1\target\debug\deps\problem_1-b0a11966b1dfda56.exe: src\main.rs
+
+C:\Users\tino\Documents\GitHub\crack-code-rs\problem-1\target\debug\deps\problem_1-b0a11966b1dfda56.d: src\main.rs
+
+src\main.rs:
