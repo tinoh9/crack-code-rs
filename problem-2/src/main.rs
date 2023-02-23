00af3d865bfd52bb1d5065860d605594f1485b2f
tinoh9 <bachhuynh959@gmail.com>
2023-02-23 16:10:05 +0700

    added solution for problem-2 (unfinished)

--- problem-2/src/main.rs
+++ problem-2/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+enum CellState {
+    Dead,
+    Alive,
+}
+
+fn calc_next_board_state(board: &Vec<Vec<CellState>>) -> Vec<Vec<CellState>> {
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::calc_next_board_state;
+    use super::CellState::*;
+
+    #[test]
+    fn test_1() {
+        #[rustfmt::skip]
+        let initial_state = vec![
+            vec![Dead,  Alive, Dead], 
+            vec![Dead,  Dead,  Alive], 
+            vec![Alive, Alive, Alive], 
+            vec![Dead,  Dead,  Dead]
+        ];
+
+        #[rustfmt::skip]
+        let final_state = vec![
+            vec![Dead,  Dead,  Dead], 
+            vec![Alive, Dead,  Alive], 
+            vec![Dead,  Alive, Alive], 
+            vec![Dead,  Alive, Dead]
+        ];
+
+        let calc_next_state = calc_next_board_state(&initial_state);
+
+        assert_eq!(final_state, calc_next_state);
+    }
 }
